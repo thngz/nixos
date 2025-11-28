b90558dee462af64cbb78feb8339309e273a9132
gkiviv <georgkivivali7@gmail.com>
2025-11-28 08:52:13 +0200

    fuzzy finder

--- programs/nvim/lua/plugins/telescope.lua
+++ programs/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,25 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
         require('telescope').setup({
             defaults = {
-                file_ignore_patterns = {'^node_modules/', '.wwwroot/', '.bin/', '.obj/'},
+                file_ignore_patterns = { '^node_modules/', '.wwwroot/', '.bin/', '.obj/' },
             }
         })
         local builtin = require('telescope.builtin')
 
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fo', builtin.current_buffer_fuzzy_find, { })
+
         vim.keymap.set('n', '<leader>rr', builtin.lsp_references, {})
         vim.keymap.set('n', '<leader>ld', builtin.diagnostics, {})
         vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 
+        vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
         vim.keymap.set('n', '<leader>gd', builtin.git_status, {})
-        vim.api.nvim_set_keymap(
-            "n",
-            "<leader>fb",
-            ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-            { noremap = true }
-        )
     end
 }
-
