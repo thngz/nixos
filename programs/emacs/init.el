8a157a6d228c434f65dd5f9631fc6bf26497e649
gkiviv <georgkivivali7@gmail.com>
2025-12-21 12:49:47 +0200

    emacs config changes

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -1,5 +1,3 @@
-;; Initialize package sources
-
 (require 'use-package)
 
 (setq make-backup-files nil) ;; stop creating ~ files
@@ -8,12 +6,10 @@
 ;; (add-hook 'after-save-hook 'lsp-format-buffer)
 
 ;; Fix tabs
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+(setq-default indent-tabs-mode nil)   ;; use spaces
+(setq-default tab-width 4)            ;; tab display width
+(setq-default indent-line-function 'indent-relative)  ;; normal indentation
 
-(setq auto-save-timeout 3)
-    (setq auto-save-interval 20)
 (scroll-bar-mode -1)
 (tool-bar-mode -1) 
 (set-fringe-mode 10)
@@ -23,7 +19,7 @@
 (setq inhibit-startup-message t) 
 
 
-(setq visible-bell t) 
+(setq visible-bell -1) 
 
 (setq global-hl-line-modes nil) 
 (column-number-mode)
@@ -31,17 +27,12 @@
 (setq display-line-numbers-type 'relative) 
 (global-display-line-numbers-mode)
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(set-face-attribute 'default nil :font "Iosevka" :height 256)
-
-(use-package all-the-icons)
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1))
+;; Looks
+(set-face-attribute 'default nil :font "Iosevka-20")
 
 (load-theme 'modus-vivendi)
-
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Finders and stuff
 (use-package vertico
@@ -51,6 +42,7 @@
   :init (setq completion-styles '(orderless)))
 
 (use-package consult)
+
 
 ;; EVIL STUFF
 (use-package evil
@@ -63,47 +55,74 @@
     (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
     (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
     (evil-define-key 'normal 'global (kbd "<leader>ff") 'consult-find)
+    (evil-define-key 'normal 'global (kbd "<leader>fo") 'consult-ripgrep)
+
     (evil-define-key 'normal 'global (kbd "<leader>gs") 'magit-status)
+
     (evil-define-key 'normal 'global (kbd "<leader>h") 'evil-window-left)
     (evil-define-key 'normal 'global (kbd "<leader>j") 'evil-window-down)
     (evil-define-key 'normal 'global (kbd "<leader>k") 'evil-window-up)
     (evil-define-key 'normal 'global (kbd "<leader>l") 'evil-window-right)
+    (evil-define-key 'normal 'global (kbd "<leader>q") 'delete-window)
+    (evil-define-key 'normal 'global (kbd "<leader>sr") #'split-window-right)
+
     (evil-define-key 'normal 'global (kbd "gd") 'xref-find-definitions)
     (evil-define-key 'normal 'global (kbd "gD") 'eglot-find-declaration)
     (evil-define-key 'normal 'global (kbd "gi") 'eglot-find-implementation)
     (evil-define-key 'normal 'global (kbd "go") 'eglot-find-typeDefinition)
     (evil-define-key 'normal 'global (kbd "gr") 'xref-find-references)
+    (evil-define-key 'normal 'global (kbd "K") 'eldoc-box-help-at-point)
     (evil-define-key 'normal 'global (kbd "<leader>ca") 'eglot-code-actions)
     (evil-define-key 'normal 'global (kbd "<leader>rn") 'eglot-rename)
     (evil-define-key 'normal 'global (kbd "<leader>vd") 'flymake-show-buffer-diagnostics)
+
+    (evil-define-key 'normal 'global (kbd "s") 'avy-goto-char-timer)
+    (evil-define-key 'normal 'global (kbd "S") 'evil-avy-goto-word-1)
+    (evil-define-key 'normal 'global (kbd "gl") 'evil-avy-goto-line)
+
+
     (evil-global-set-key 'motion "j" 'evil-next-visual-line)
     (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
     (evil-set-leader nil (kbd "SPC"))
     (evil-set-initial-state 'messages-buffer-mode 'normal)
     (evil-set-initial-state 'dashboard-mode 'normal)
-    (evil-set-initial-state 'bs-mode 'emacs)
-    (evil-set-initial-state 'term-mode 'emacs))
+    (evil-set-initial-state 'bs-mode 'emacs))
+
+(use-package avy
+  :config
+  (setq avy-timeout-seconds 0.2
+        avy-style 'at-full))
 
 
-(use-package evil-collection
- :after evil
- :config
- (evil-collection-init))
+(use-package evil-collection :after evil :config (evil-collection-init))
 
 ;; CODE STUFF
-(use-package eglot
-  :hook ((python-ts-mode . eglot-ensure)
-         (rust-ts-mode . eglot-ensure)
-         (go-ts-mode . eglot-ensure)))
 
+
+
+(use-package eglot
+  :defer t
+  :hook ((python-mode . eglot-ensure)
+         (go-mode . eglot-ensure)))
+
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 1))
+
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(global-company-mode 1)
+(electric-pair-mode 1)
 (use-package magit
     :commands (magit-status magit-get-current-branch)
     :custom
     (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package eldoc-box
-  :ensure t
-  :hook (eglot-managed-mode . eldoc-box-hover-mode))
+(use-package eldoc-box :ensure t)
 
 (use-package treesit-auto
   :ensure t
